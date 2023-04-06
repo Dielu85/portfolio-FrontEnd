2754f4941b9724fef587adc6fee92cd1b9528986
Dielu85 <Diegogomez1521@gmail.com>
2023-04-06 19:23:24 -0300

    update

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { GraficoComponent } from './componentes/habilidades/grafico/grafico.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    NavBarComponent
+    NavBarComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    GraficoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
