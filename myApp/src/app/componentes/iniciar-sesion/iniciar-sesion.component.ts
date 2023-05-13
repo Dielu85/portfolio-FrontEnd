6bf8c5fea727a253b54353afd45358638afc3099
Dielu85 <Diegogomez1521@gmail.com>
2023-05-13 08:46:33 -0300

    add login

--- myApp/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ myApp/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,45 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
   styleUrls: ['./iniciar-sesion.component.css']
 })
-export class IniciarSesionComponent implements OnInit{
-  form:FormGroup;
-  constructor (private formBuilder: FormBuilder, private autenticacionService:AutenticacionService, private ruta:Router ){
-    this.form=this.formBuilder.group(
-      {
-        email:['',[Validators.required,Validators.email]],
-        password:['',[Validators.required,Validators.minLength(8)]],
-        deviceInfo:this.formBuilder.group({
-          deviceId:["17867868768"],
-          deviceType:["DEVICE_TYPE_ANDROID"],
-          NotificationToken:["67657575eececc34"]
-        })
-    }
-    )
-  }
+export class IniciarSesionComponent implements OnInit {
+  form: FormGroup;
 
-  ngOnInit(): void {
-    
-  }
-  get Email()
-  {
-  return this.form.get('email');
- }
-
-  get Password(){
-    return this.form.get('password');
-  }
-  onEnviar(event:Event){
-    event.preventDefault;
-    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data =>{
-      console.log("DATA: " + JSON.stringify(data));
-      this.ruta.navigate(['/portfolio']);
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
+  }
+
+  ngOnInit() {}
+
+  login() {
+      if (this.form.valid) {
+        const email = this.form.get('email')?.value;
+        const password = this.form.get('password')?.value;
+    
+        if (email === 'Diegogomez1521@gmail.com' && password === 'YoProgramo') {
+          this.router.navigate(['/portfolio']);
+        } else  {
+      }
+    }
+  }
+
+  get Email() {
+    return this.form.get('email');
+  }
+
+  get Password() {
+    return this.form.get('password');
   }
 }
